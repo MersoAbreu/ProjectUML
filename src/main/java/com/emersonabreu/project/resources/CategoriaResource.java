d01a5a81674d191a5d849231d87f7ef5631e97d5
Emerson de Abreu <mersoabreu@gmail.com>
2021-01-08 16:05:28 -0300

    Testando classe Domain categoria

--- src/main/java/com/emersonabreu/project/resources/CategoriaResource.java
+++ src/main/java/com/emersonabreu/project/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.emersonabreu.project.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.emersonabreu.project.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String Listar() {
-		return "Rest funcionando";
+	public List<Categoria> Listar() {
+		Categoria cat1 = new Categoria();
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		cat1.setId(1);
+		cat1.setNome("Informatica");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 
 }
